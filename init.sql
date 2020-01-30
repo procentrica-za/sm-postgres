0fa3a53af7081cf29df6ffe006ad2b479a53eabd
ChaneBornman <chane@procentrica.co.za>
2020-01-30 09:57:13 +0200

    Updated Init.sql file, now initializes db correctly on postgres docker image.

--- init.sql
+++ init.sql
@@ -1,182 +1,211 @@
 CREATE TABLE public.Image (
-    ID uuid PRIMARY KEY,
-    PathString Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    ID uuid PRIMARY KEY NOT NULL,
+    PathString Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.AdvertisementImage (
-    AdvertisingID uuid,
-    ImageID uuid,
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    AdvertisingID uuid NOT NULL,
+    ImageID uuid NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp,
     PRIMARY KEY(AdvertisingID, ImageID)
 );
 CREATE TABLE public.User (
-    ID uuid PRIMARY KEY,
-    Username Varchar(50),
-    Password Varchar(50),
-    Name Varchar(30),
-    Surname Varchar(30),
-    Email Varchar(50),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    ID uuid PRIMARY KEY NOT NULL,
+    Username Varchar(50) NOT NULL,
+    Password Varchar(50) NOT NULL,
+    Name Varchar(30) NOT NULL,
+    Surname Varchar(30) NOT NULL,
+    Email Varchar(50) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
+    ModifiedDateTime timestamp
+);
+CREATE TABLE public.Advertisement (
+    ID uuid PRIMARY KEY NOT NULL,
+    UserID uuid NOT NULL,
+    CONSTRAINT adertisementuserfkey FOREIGN KEY (UserID)
+        REFERENCES public.User (ID) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    AdvertisementType Varchar(3) NOT NULL,
+    EntityID uuid NOT NULL,
+    Price Decimal NOT NULL,
+    Description Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.Rating (
-    ID uuid PRIMARY KEY,
-    AdvertisementID uuid,
-    BuyerID uuid,
-    SellerID uuid,
+    ID uuid PRIMARY KEY NOT NULL,
+    AdvertisementID uuid NOT NULL,
+    SellerID uuid NOT NULL,
+    BuyerID uuid NOT NULL,
+    CONSTRAINT ratingsellerfkey FOREIGN KEY (SellerID)
+        REFERENCES public.User (ID) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT ratingbuyerfkey FOREIGN KEY (BuyerID)
+        REFERENCES public.User (ID) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT ratingadvertisementfkey FOREIGN KEY (AdvertisementID)
+        REFERENCES public.Advertisement (ID) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
     BuyerRating int,
     SellerRating int,
     BuyerComments Varchar(255),
     SellerComments Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.AdvertisementType (
-    Code Varchar(3),
-    Name Varchar(3),
-    Description Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
-    ModifiedDateTime timestamp
-);
-CREATE TABLE public.Notes (
-    ModuleCode Varchar(50),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
-    ModifiedDateTime timestamp
-);
-CREATE TABLE public.Textbook (
-    ID uuid PRIMARY KEY,
-    CONSTRAINT textbookmodulefkey FOREIGN KEY (ID)
-        REFERENCES Module (ID) MATCH SIMPLE
-        ON UPDATE NO ACTION ON DELETE NO ACTION,
-    Name Varchar(255),
-    Edition Varchar(30),
-    Quality Varchar(255),
-    Author Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean,
+    Code Varchar(3) NOT NULL,
+    Name Varchar(3) NOT NULL,
+    Description Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.Institution (
-    ID uuid PRIMARY KEY,
-    Name Varchar(50),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean,
+    ID uuid PRIMARY KEY NOT NULL,
+    Name Varchar(50) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.Faculty (
-    ID uuid PRIMARY KEY,
-    CONSTRAINT facultyinstitutionfkey FOREIGN KEY (ID)
-        REFERENCES Institution (ID) MATCH SIMPLE
+    ID uuid PRIMARY KEY NOT NULL,
+    InstitutionID uuid NOT NULL,
+    CONSTRAINT facultyinstitutionfkey FOREIGN KEY (InstitutionID)
+        REFERENCES public.Institution (ID) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION,
-    Name Varchar(50),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean,
+    Name Varchar(50) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.Module (
-    ID uuid PRIMARY KEY,
-    CONSTRAINT modulefacultyfkey FOREIGN KEY (ID)
-        REFERENCES Faculty (ID) MATCH SIMPLE
+    ID uuid PRIMARY KEY NOT NULL,
+    FacultyID uuid NOT NULL,
+    CONSTRAINT modulefacultyfkey FOREIGN KEY (FacultyID)
+        REFERENCES public.Faculty (ID) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION,
-    Name Varchar(50),
-    ModuleCode Varchar(50),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean,
+    Name Varchar(50) NOT NULL,
+    ModuleCode Varchar(50) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
+CREATE TABLE public.Notes (
+    ID uuid PRIMARY KEY NOT NULL,
+    ModuleID uuid NOT NULL,
+    CONSTRAINT notesmodulefkey FOREIGN KEY (ModuleID)
+        REFERENCES public.Module (ID) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
+    ModifiedDateTime timestamp
+);
+CREATE TABLE public.Textbook (
+    ID uuid PRIMARY KEY NOT NULL,
+    ModuleID uuid NOT NULL,
+    CONSTRAINT textbookmodulefkey FOREIGN KEY (ModuleID)
+        REFERENCES public.Module (ID) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    Name Varchar(255) NOT NULL,
+    Edition Varchar(30),
+    Quality Varchar(255) NOT NULL,
+    Author Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
+    ModifiedDateTime timestamp
+);
+
 CREATE TABLE public.Tutor (
-    ID uuid PRIMARY KEY,
-    Subject Varchar(50),
-    YearCompleted Varchar(4),
-    ModuleCode Varchar(50),
-    Venue Varchar(100),
-    NotesIncluded Boolean,
-    Terms Varchar(20),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean,
-    ModifiedDateTime timestamp
-);
-CREATE TABLE public.Advertisement (
-    ID uuid PRIMARY KEY,
-    CONSTRAINT adertisementuserfkey FOREIGN KEY (ID)
-        REFERENCES User (ID) MATCH SIMPLE
+    ID uuid PRIMARY KEY NOT NULL,
+    Subject Varchar(50) NOT NULL,
+    YearCompleted Varchar(4) NOT NULL,
+    ModuleID uuid NOT NULL,
+    CONSTRAINT tutormodulefkey FOREIGN KEY (ModuleID)
+        REFERENCES public.Module (ID) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION,
-    AdvertisementType Varchar(3),
-    EntityID uuid,
-    Price Decimal,
-    Description Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean,
+    Venue Varchar(100) NOT NULL,
+    NotesIncluded Boolean NOT NULL,
+    Terms Varchar(20) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.AccomodationType (
-    Code varchar(3) PRIMARY KEY,
-    Description Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    Code Varchar(3) PRIMARY KEY NOT NULL,
+    Name Varchar(50) NOT NULL,
+    Description Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.Accomodation (
-    ID uuid PRIMARY KEY,
-    CONSTRAINT accomodationaccomodationtypefkey FOREIGN KEY (ID)
-        REFERENCES AccomodationType (ID) MATCH SIMPLE
+    ID uuid PRIMARY KEY NOT NULL,
+    AccomodationTypeCode varchar(3) NOT NULL,
+    CONSTRAINT accomodationaccomodationtypefkey FOREIGN KEY (AccomodationTypeCode)
+        REFERENCES public.AccomodationType (Code) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION,
-
-    CONSTRAINT accomodationinstitutionfkey FOREIGN KEY (ID)
-        REFERENCES Institution (ID) MATCH SIMPLE
+    InstitutionID uuid NOT NULL,
+    CONSTRAINT accomodationinstitutionfkey FOREIGN KEY (InstitutionID)
+        REFERENCES public.Institution (ID) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION,
-    Location Varchar(255),
-    DistanceToCampus Varchar(20),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    Location Varchar(255) NOT NULL,
+    DistanceToCampus Varchar(20) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.Feature (
-    ID uuid PRIMARY KEY,
-    Name Varchar(100),
-    Description Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    ID uuid PRIMARY KEY NOT NULL,
+    Name Varchar(100) NOT NULL,
+    Description Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.AccomodationFeature (
-    AccomodationID uuid,
-    FeatureID uuid,
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    AccomodationID uuid NOT NULL,
+    FeatureID uuid NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp,
     PRIMARY KEY(AccomodationID, FeatureID)
 );
 CREATE TABLE public.EntityCode (
-    Code Varchar(3) PRIMARY KEY,
-    Name Varchar(25),
-    Description Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    Code Varchar(3) PRIMARY KEY NOT NULL,
+    Name Varchar(25) NOT NULL,
+    Description Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.EventCode (
-    Code Varchar(3) PRIMARY KEY,
-    Name Varchar(25),
-    Description Varchar(255),
-    CreatedDateTime timestamp,
-    IsDeleted Boolean DEFAULT(0),
+    Code Varchar(3) PRIMARY KEY NOT NULL,
+    Name Varchar(25) NOT NULL,
+    Description Varchar(255) NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
 CREATE TABLE public.Event (
-    ID uuid PRIMARY KEY,
-    EventCode Varchar(3),
-    EntityCode Varchar(3),
-    EntityID uuid,
-    CreatedDateTime timestamp,
-    IsDeleted Boolean,
+    ID uuid PRIMARY KEY NOT NULL,
+    EventCode varchar(3) NOT NULL,
+    EntityCode varchar(3) NOT NULL,
+    CONSTRAINT eventeventcodefkey FOREIGN KEY (EventCode)
+        REFERENCES public.EventCode (Code) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    CONSTRAINT evententitycodefkey FOREIGN KEY (EntityCode)
+        REFERENCES public.EntityCode (Code) MATCH SIMPLE
+        ON UPDATE NO ACTION ON DELETE NO ACTION,    
+    EntityID uuid NOT NULL,
+    CreatedDateTime timestamp NOT NULL,
+    IsDeleted Boolean DEFAULT(false),
     ModifiedDateTime timestamp
 );
